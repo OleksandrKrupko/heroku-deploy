dacef85b21248faa7657ca672885f09e50b7109e
Oleksandr Krupko <Oleksandr_Krupko@epam.com>
2019-03-25 19:18:04 +0200

    changed port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000
+const PORT = 80
 
 app.use(express.static(path.join(__dirname, '../client/')));
+app.get('/', (req, res) => res.send('SUCCESS'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
