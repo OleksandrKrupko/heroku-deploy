9cdf8f85f50719ae4a886f25af76ad4f8afae15a
Oleksandr Krupko <Oleksandr_Krupko@epam.com>
2019-03-25 19:21:36 +0200

    set auto port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 80
+const PORT = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, '../client/')));
 app.get('/', (req, res) => res.send('SUCCESS'));
